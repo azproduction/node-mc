bad3662f7e95eaccebb0db6b5614ff9a07828ab2
Mikhail Davydov <azazel.private@gmail.com>
2015-05-10 23:59:05 +0200

    raf redraw

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -13,15 +13,9 @@ export default class App extends React.Component {
     }
 
     componentDidMount() {
-        this._renderAnsi();
+        this._setupRepaint();
         this.props.flux.getActions('tabs').changeDir('leftPanel', '~');
         this.props.flux.getActions('tabs').changeDir('rightPanel', '~');
-    }
-
-    componentDidUpdate() {
-        setTimeout(() => {
-            this._renderAnsi();
-        }, 0);
     }
 
     _applicationChange() {
@@ -30,13 +24,50 @@ export default class App extends React.Component {
         });
     }
 
-    _renderAnsi() {
-        var htmlElement = this.refs.dom.getDOMNode();
-        var element = new TuiElement(htmlElement);
-        var serializedElement = compressBox(element.toArray());
+    _setupRepaint() {
+        var repaint = () => {
+            var htmlElement = React.findDOMNode(this.refs.dom);
+            var element = new TuiElement(htmlElement);
 
-        console.log.apply(console, render.chrome(serializedElement));
-        this.props.flux.getActions('render').renderAnsi(render.ansi(serializedElement));
+            var serializedElement = element.toArray();
+            serializedElement = compressBox(serializedElement);
+
+            var ansi = render.ansi(serializedElement);
+
+            // console.log.apply(console, render.chrome(serializedElement));
+            this.props.flux.getActions('render').renderAnsi(ansi);
+            requestAnimationFrame(repaint);
+        };
+        repaint();
+    }
+
+    _renderFileList(fileList) {
+        return fileList.slice(0, 10).map((file, key) => {
+            var mtime = new Date(file.stat.mtime).toLocaleDateString();
+            return (
+                <li className="file" key={key}>
+                    <div className="file__name">{file.name}</div>
+                    <div className="file__size">{file.stat.size}</div>
+                    <div className="file__time">{mtime}</div>
+                </li>
+            );
+        });
+    }
+
+    _renderPanel(panel) {
+        return (
+            <div className="panel">
+                <div className="panel__caption">{panel.dirName}</div>
+                <ul className="file-list">
+                    <li className="header">
+                        <div className="header__name">Name</div>
+                        <div className="header__size">Size</div>
+                        <div className="header__time">MTime</div>
+                    </li>
+                    {this._renderFileList(panel.fileList)}
+                </ul>
+            </div>
+        );
     }
 
     render() {
@@ -48,18 +79,66 @@ export default class App extends React.Component {
         return (
             <div className="b-app">
                 <div className="b-app__dom" ref="dom" style={windowSizes}>
-                    <div className="b-app__content">
-                        <div onClick={this._applicationChange.bind(this)}
-                             className="b-app__block">
-                            {this.state.frame || 'Click Me'}
+                    <div className="mc">
+                        <ul className="menu">
+                            <li className="menu__item"><span className="menu__item-shortcut">L</span>eft</li>
+                            <li className="menu__item"><span className="menu__item-shortcut">F</span>ile</li>
+                            <li className="menu__item"><span className="menu__item-shortcut">C</span>ommand</li>
+                            <li className="menu__item"><span className="menu__item-shortcut">O</span>ptions</li>
+                            <li className="menu__item"><span className="menu__item-shortcut">R</span>ight</li>
+                        </ul>
+                        <div className="content">
+                            {this.props.leftPanel ? this._renderPanel(this.props.leftPanel.toJS()) : null}
+                            {this.props.rightPanel ? this._renderPanel(this.props.rightPanel.toJS()) : null}
                         </div>
-                        <input type="text"
-                               defaultValue={this.state.frame}
-                               onBlur={this._applicationChange.bind(this)}
-                               onFocus={this._applicationChange.bind(this)}
-                               onChange={this._applicationChange.bind(this)}
-                               autoFocus={true}
-                               className="b-app__input"/>
+                        <div className="console">
+                            <input className="console__input"
+                                   type="text"
+                                   defaultValue="~/Documents/html-cli$ node index.js"
+                                   autofocus />
+                        </div>
+                        <ul className="controls">
+                            <li className="control">
+                                <div className="control__number">1</div>
+                                <div className="control__name">Help</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">2</div>
+                                <div className="control__name">Menu</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">3</div>
+                                <div className="control__name">View</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">4</div>
+                                <div className="control__name">Edit</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">5</div>
+                                <div className="control__name">Copy</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">6</div>
+                                <div className="control__name">RenMov</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">7</div>
+                                <div className="control__name">Mkdir</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">8</div>
+                                <div className="control__name">Delete</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">9</div>
+                                <div className="control__name">PullDn</div>
+                            </li>
+                            <li className="control">
+                                <div className="control__number">10</div>
+                                <div className="control__name">Quit</div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <pre className="b-app__cli" ref="cli"></pre>
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,6 +2,8 @@ import koa from 'koa.io';
 import router from 'koa-router';
 import json from 'koa-json';
 import env from '../../../shared/env';
+import {readdirSync, statSync} from 'fs';
+import {join} from 'path';
 
 var app = koa();
 
@@ -9,8 +11,14 @@ app.use(json());
 app.use(router(app));
 
 app.get('/ls', function *() {
+    var dir = process.env.HOME;
     this.body = {
-        list: ['.', '..']
+        list: readdirSync(dir).map((name) => {
+            return {
+                name,
+                stat: statSync(join(dir, name))
+            };
+        })
     };
 });
 
