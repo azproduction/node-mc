6cff592dfabbef60256708604c1ccd2f79e2cfd5
Mikhail Davydov <mdavydov@productivemobile.com>
2015-06-05 19:32:38 +0200

    default port is 8181

--- server/lib/cli-options.js
+++ server/lib/cli-options.js
@@ -4,7 +4,7 @@ import pkg from '../../package.json';
 export default function getCliOptions(process) {
     return commander
         .version(pkg.version)
-        .option('-p, --port [port]', 'Rendered port', parseInt, 8080)
+        .option('-p, --port [port]', 'Rendered port', parseInt, 8181)
         .option('-h, --host [host]', 'Render host', 'localhost')
         .option('-c, --client [browser]', 'Client', 'default')
         .option('-e, --node-env [environment]', 'Environment', process.env.NODE_ENV || 'development')
